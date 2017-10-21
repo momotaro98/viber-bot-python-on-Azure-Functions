b7ec45977eed6341fc74a561af45596e58a802e2
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2017-10-22 02:00:38 +0900

    display current directory

--- EmailTriggerToViber/run.py
+++ EmailTriggerToViber/run.py
@@ -2,24 +2,23 @@ import os
 import json
 
 import sys
-print(sys.path)
+print("sys.path", sys.path)
+print("current directory: ", os.path.dirname(os.path.realpath(__file__)))
 
-'''
-from viberbot_lib.viberbot import Api
-from viberbot_lib.viberbot.api.bot_configuration import BotConfiguration
-from viberbot_lib.viberbot.api.messages.text_message import TextMessage
-from viberbot_lib.viberbot.api.viber_requests import ViberConversationStartedRequest
-from viberbot_lib.viberbot.api.viber_requests import ViberFailedRequest
-from viberbot_lib.viberbot.api.viber_requests import ViberMessageRequest
-from viberbot_lib.viberbot.api.viber_requests import ViberSubscribedRequest
-from viberbot_lib.viberbot.api.viber_requests import ViberUnsubscribedRequest
+from .viberbot_lib.viberbot import Api
+from .viberbot_lib.viberbot.api.bot_configuration import BotConfiguration
+from .viberbot_lib.viberbot.api.messages.text_message import TextMessage
+from .viberbot_lib.viberbot.api.viber_requests import ViberConversationStartedRequest
+from .viberbot_lib.viberbot.api.viber_requests import ViberFailedRequest
+from .viberbot_lib.viberbot.api.viber_requests import ViberMessageRequest
+from .viberbot_lib.viberbot.api.viber_requests import ViberSubscribedRequest
+from .viberbot_lib.viberbot.api.viber_requests import ViberUnsubscribedRequest
 
 viber = Api(BotConfiguration(
   name='InternalOpeBot',
   avatar='',
   auth_token=os.getenv('VIBER_API_KEY') or 'default'
 ))
-'''
 
 postreqdata = json.loads(open(os.environ['req']).read())
 print(postreqdata)
