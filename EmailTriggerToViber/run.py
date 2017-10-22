97c05a28bb6c8b16314f88974ac8d67e080179eb
Shintaro Ikeda <ikenshirogivenup98@gmail.com>
2017-10-22 11:56:02 +0900

    Fix to append my library's path to python path

--- EmailTriggerToViber/run.py
+++ EmailTriggerToViber/run.py
@@ -1,18 +1,27 @@
 import os
+import os.path
+import sys
 import json
 
-import sys
-print("sys.path", sys.path)
-print("current directory: ", os.path.dirname(os.path.realpath(__file__)))
+# Print Python version
+import platform
+message = "Using Python '{0}'".format(platform.python_version())
+print(message)
 
-from .viberbot_lib.viberbot import Api
-from .viberbot_lib.viberbot.api.bot_configuration import BotConfiguration
-from .viberbot_lib.viberbot.api.messages.text_message import TextMessage
-from .viberbot_lib.viberbot.api.viber_requests import ViberConversationStartedRequest
-from .viberbot_lib.viberbot.api.viber_requests import ViberFailedRequest
-from .viberbot_lib.viberbot.api.viber_requests import ViberMessageRequest
-from .viberbot_lib.viberbot.api.viber_requests import ViberSubscribedRequest
-from .viberbot_lib.viberbot.api.viber_requests import ViberUnsubscribedRequest
+# Add library path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'site-packages')))
+
+# Print path
+print("sys.path", sys.path)
+
+from viberbot import Api
+from viberbot.api.bot_configuration import BotConfiguration
+from viberbot.api.messages.text_message import TextMessage
+from viberbot.api.viber_requests import ViberConversationStartedRequest
+from viberbot.api.viber_requests import ViberFailedRequest
+from viberbot.api.viber_requests import ViberMessageRequest
+from viberbot.api.viber_requests import ViberSubscribedRequest
+from viberbot.api.viber_requests import ViberUnsubscribedRequest
 
 viber = Api(BotConfiguration(
   name='InternalOpeBot',
